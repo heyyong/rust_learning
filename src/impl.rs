007a8b07674d70ba551c3fcc118dbab3f24aeca0
caoyong <caoyong@bytedance.com>
2018-10-27 21:52:13 +0800

    add impl
    Change-Id: I61a0a2e4baf1d033ab3970a98c3a63e02b51a44b

--- src/impl.rs
+++ src/impl.rs
@@ -8,6 +8,17 @@ impl Rectangle {
   fn area(&self) -> u32 {
     self.width * self.height
   }
+
+  fn can_hold(&self, other: &Rectangle) -> bool {
+    self.width > other.width && self.height > other.height
+  }
+
+  fn square(size: u32) -> Rectangle {
+    Rectangle {
+      width: size,
+      height: size
+    }
+  }
 }
 
 fn main() {
@@ -16,8 +27,23 @@ fn main() {
     height: 50
   };
 
+  let rect2 = Rectangle {
+    width: 10,
+    height: 40
+  };
+
   println!(
     "The area of the rectangle is {} square pixels.",
     rect1.area()
+  );
+
+  println!(
+    "Can rect1 hold rect2: {}",
+    rect1.can_hold(&rect2)
+  );
+
+  println!(
+    "associated func: {:?}",
+    Rectangle::square(20)
   );
 }
