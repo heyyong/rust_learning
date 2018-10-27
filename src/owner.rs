8d6bce0fd23afe2d6cbc23445280a62016e04eb1
caoyong <caoyong@bytedance.com>
2018-10-27 20:21:03 +0800

    update owner
    Change-Id: Ic523ee8cb2033df7b30a628fcfeefd791eec3d36

--- src/owner.rs
+++ src/owner.rs
@@ -10,4 +10,36 @@ fn main() {
 
   // s1 moved here
   println!("{}, world", s2);
+
+  let s3 = s2.clone();
+
+  println!("{}", s3);
+
+  let s5 = "hello";
+  let s6 = s5;
+  println!("{}", s5);
+
+  let str = String::from("hello");
+
+  takes_ownership(str);
+
+  let s2 = String::from("hello");
+
+  let s3 = take_and_give_back(s2);
+
+  println!("{}", s3);
+}
+
+fn take_and_give_back(a_string: String) -> String {
+  a_string
+}
+
+fn gives_ownership() -> String {
+  let some_string = String::from("hello");
+
+  some_string
+}
+
+fn takes_ownership(something: String) {
+  println!("{}", something);
 }
