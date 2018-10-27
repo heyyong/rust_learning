56965dbc2c27fdcaf010661580f56acd2bdca835
caoyong <caoyong@bytedance.com>
2018-10-27 16:59:38 +0800

    add condition
    Change-Id: Ie8e72fd4902ae68b5bc7264097533f59f61f50e8

---
+++ src/condition.rs
@@ -0,0 +1,11 @@
+fn main() {
+  let number = 6;
+
+  if number % 4 == 0 {
+    println!("condition was divisible by 4");
+  } else if number % 3 == 0 {
+    println!("number was divisible by 3");
+  } else {
+    println!("not condition");
+  }
+}
