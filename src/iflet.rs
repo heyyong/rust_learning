d1bec2d041877db7cdc964c5719781bfd0423b9d
caoyong <caoyong@bytedance.com>
2018-10-28 13:26:48 +0800

    add iflet
    Change-Id: I8f95bbf5344e0e8ff3afb58a324ef9623bc4726f

--- src/iflet.rs
+++ src/iflet.rs
@@ -1,3 +1,27 @@
+enum Coin {
+  Penny,
+  Nickel,
+  Dime,
+  Quarter(UsState),
+}
+
+#[derive(Debug)]
+enum UsState {
+  Alabama,
+  Alaska
+}
+
 fn main() {
-  
+  let some_u8_value = Some(0u8);
+  if let Some(3) = some_u8_value {
+    println!("three");
+  }
+
+  let mut count = 0;
+  let coin = Coin::Quarter(UsState::Alaska);
+  if let Coin::Quarter(state) = coin {
+    println!("State qurter from {:?}!", state);
+  } else {
+    count += 1;
+  }
 }
