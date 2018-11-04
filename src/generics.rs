405b18052840aedafc0f8eca0652bfffde10a8b3
caoyong <caoyong@bytedance.com>
2018-11-04 14:19:39 +0800

    impl trait bound
    Change-Id: I26671101626ab4b1a8b29bc0e25b464803d992ed

--- src/generics.rs
+++ src/generics.rs
@@ -14,21 +14,21 @@ fn main() {
   let integer = Point {
     x: 5,
     y: 10
-  }
+  };
 
   let float = Point {
     x: 1.0,
     y: 4.0
-  }
+  };
 
-  println!("p.x = {}", p.x())
+  println!("p.x = {}", float.x());
 
   let number_list = vec![34, 50, 25, 100, 65];
 
   println!("The largest number is {}", find_largest(&number_list));
 }
 
-fn find_largest<T>(list: &[T]) -> T {
+fn find_largest<T: PartialOrd + Copy>(list: &[T]) -> T {
   let mut largest = list[0];
 
   for &number in list.iter() {
