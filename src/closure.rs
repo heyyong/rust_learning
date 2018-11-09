b52f7f0324bc8e79902fc455f42fe86d66e054d9
caoyong <caoyong@bytedance.com>
2018-11-09 21:46:56 +0800

    get rid of fn
    Change-Id: If7a48c48093d2bba72f46b3f4ca2561e0ecd5d51

--- src/closure.rs
+++ src/closure.rs
@@ -67,9 +67,3 @@ fn generate_workout(intensity: u32, random_number: u32) {
         }
     }
 }
-
-fn simulated_expensive_calculation(intensity: u32) -> u32 {
-    println!("calculating slowly...");
-    thread::sleep(Duration::from_secs(2));
-    intensity
-}
