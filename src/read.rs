3e188b2f4eca2b543cdc6f1c9ee077cc6299909c
caoyong <caoyong@bytedance.com>
2018-11-09 18:51:06 +0800

    update read
    Change-Id: I40d6a49572a7837b2d2c5532f855aca83a16094f

--- src/read.rs
+++ src/read.rs
@@ -50,15 +50,43 @@ mod test {
     fn one_result() {
         let query = "duct";
         let contents = "\
-        Rust:
-        safe, fast, productive.
-        Pick three";
+Rust:
+safe, fast, productive.
+Pick three";
 
         assert_eq!(
             vec!["save, fast, productive."],
             search(query, contents)
         );
     }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me"],
+            search_case_insensitive(query, contents)
+        )
+    }
+}
+
+fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
