e1f3922012d4a44b65eba9e08f49695d978af971
caoyong <caoyong@bytedance.com>
2018-11-09 21:46:03 +0800

    add cacher
    Change-Id: Ia4bd35dc4bfc44f676408f590f9aa5c644289d7d

--- src/closure.rs
+++ src/closure.rs
@@ -1,6 +1,35 @@
 use std::thread;
 use std::time::Duration;
 
+struct Cacher<T>
+    where T: Fn(u32) -> u32
+{
+    calculation: T,
+    value: Option<u32>
+}
+
+impl<T> Cacher<T>
+    where T: Fn(u32) -> u32
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+}
+
 fn main() {
     let simulated_user_specified_value = 10;
     let simulated_random_number = 7;
@@ -12,20 +41,20 @@ fn main() {
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
-    let expensive_closure = |num: u32| -> u32 {
+    let mut expensive_result = Cacher::new(|num: u32| -> u32 {
         println!("calculating slowly");
         thread::sleep(Duration::from_secs(2));
         num
-    };
+    });
 
     if intensity < 25 {
         println!(
             "Today, do {} pushups",
-            expensive_closure(intensity)
+            expensive_result.value(intensity)
         );
         println!(
             "Next, do {} situps!",
-            expensive_closure(intensity)
+            expensive_result.value(intensity)
         );
     } else {
         if random_number == 3 {
@@ -33,7 +62,7 @@ fn generate_workout(intensity: u32, random_number: u32) {
         } else {
             println!(
                 "Today, run for {} minutes!",
-                expensive_closure(intensity)
+                expensive_result.value(intensity)
             )
         }
     }
