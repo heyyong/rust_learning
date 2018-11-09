2413e8bc9b93072a65966fdc7ff785b14922e103
caoyong <caoyong@bytedance.com>
2018-11-09 21:47:40 +0800

    format cacher
    Change-Id: I715fb0f8868470918f6a748b9a4e8f83fac5f7c5

--- src/closure.rs
+++ src/closure.rs
@@ -1,6 +1,16 @@
 use std::thread;
 use std::time::Duration;
 
+fn main() {
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
+
+    generate_workout(
+        simulated_user_specified_value,
+        simulated_random_number
+    )
+}
+
 struct Cacher<T>
     where T: Fn(u32) -> u32
 {
@@ -28,16 +38,6 @@ impl<T> Cacher<T>
             }
         }
     }
-}
-
-fn main() {
-    let simulated_user_specified_value = 10;
-    let simulated_random_number = 7;
-
-    generate_workout(
-        simulated_user_specified_value,
-        simulated_random_number
-    )
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
