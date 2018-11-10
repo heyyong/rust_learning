59bf2627eb39996ac803a40d5a21496c3246130f
caoyong <caoyong@bytedance.com>
2018-11-10 21:21:14 +0800

    add iter test
    Change-Id: Ibe82d1b1d8b8ce5a81d1caf0f9a5f728676be275

--- src/iter.rs
+++ src/iter.rs
@@ -1,9 +1,30 @@
-fn main() {
-    let v1 = vec![1, 2, 3];
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
 
-    let v1_iter = v1.iter();
+fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter()
+        .filter(|s| s.size == shoe_size)
+        .collect()
+}
 
-    for val in v1_iter {
-        println!("Got: {}", val);
-    }
+#[test]
+fn filter_by_size() {
+    let shoes = vec![
+        Shoe { size: 10, style: String::from("sneaker") },
+        Shoe { size: 13, style: String::from("sandal") },
+        Shoe { size: 10, style: String::from("boot") },
+    ];
+
+    let in_my_size = shoes_in_my_size(shoes, 10);
+
+    assert_eq!(
+        in_my_size,
+        vec![
+            Shoe { size: 10, style: String::from("sneaker") },
+            Shoe { size: 10, style: String::from("boot") }
+        ]
+    )
 }
