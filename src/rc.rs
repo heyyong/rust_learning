964b74b580d4feb72bd13fe109392dc3c56a4c54
caoyong <caoyong@bytedance.com>
2018-11-11 21:37:14 +0800

    add rc
    Change-Id: Ibae406a6c06dbaa8ddee45026bb92257ea5a2744

---
+++ src/rc.rs
@@ -0,0 +1,14 @@
+use List::{Cons, Nil};
+use std::rc::Rc;
+
+enum List {
+    Cons(i32, Rc<List>),
+    Nil
+}
+
+fn main() {
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+
+    let b = Cons(3, Rc::clone(&a));
+    let c = Cons(4, Rc::clone(&a));
+}
