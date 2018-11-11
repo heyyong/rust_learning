d27214b8cfc103b126684397d0f1fec30bbf3547
caoyong <caoyong@bytedance.com>
2018-11-11 20:58:03 +0800

    update box
    Change-Id: I11444db4b21ef8352e6446ce0b764d3bb1dc82af

--- src/box.rs
+++ src/box.rs
@@ -1,4 +1,25 @@
+use std::ops::Deref;
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
 fn main() {
-    let b = Box::new(5);
-    println!("b={}", b);
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
 }
