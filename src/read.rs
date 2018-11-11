b21d9188dcc426a096ec935c43d0f35215594212
caoyong <caoyong@bytedance.com>
2018-11-11 12:41:55 +0800

    update search in read by iter
    Change-Id: I9786c6bc436e6070b9824f694645a247237e73fd

--- src/read.rs
+++ src/read.rs
@@ -93,12 +93,7 @@ fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents.lines()
+            .filter(|line| line.contains(query))
+            .collect()
 }
