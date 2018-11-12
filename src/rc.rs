b9dfb6c7aae4d2935f77d2efc611548ed6f8796c
caoyong <caoyong@bytedance.com>
2018-11-12 11:30:13 +0800

    multiple mut ref
    Change-Id: I3505c1b7d1a74537e932b85f7d26f7e42edd1202

--- src/rc.rs
+++ src/rc.rs
@@ -1,14 +1,29 @@
 use List::{Cons, Nil};
 use std::rc::Rc;
+use std::cell::RefCell;
 
+#[derive(Debug)]
 enum List {
-    Cons(i32, Rc<List>),
+    Cons(Rc<RefCell<i32>>, Rc<List>),
     Nil
 }
 
 fn main() {
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    // let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    //
+    // let b = Cons(3, Rc::clone(&a));
+    // let c = Cons(4, Rc::clone(&a));
 
-    let b = Cons(3, Rc::clone(&a));
-    let c = Cons(4, Rc::clone(&a));
+    let value = Rc::new(RefCell::new(5));
+
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+    let b = Cons(Rc::new(RefCell::new(6)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(10)), Rc::clone(&a));
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
 }
