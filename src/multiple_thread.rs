0ac3da4e7766d7c1a93615fb59b99a80fba0793d
caoyong <caoyong@bytedance.com>
2018-11-13 19:30:23 +0800

    finish multi thread
    Change-Id: I4651e5a827bde6820a282a7bc85921b33ddce814

---
+++ src/multiple_thread.rs
@@ -0,0 +1,12 @@
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+  let v = vec![1, 2, 3];
+
+  let handle = thread::spawn(move || {
+    println!("Here's a vector: {:?}", v);
+  });
+
+  handle.join().unwrap();
+}
